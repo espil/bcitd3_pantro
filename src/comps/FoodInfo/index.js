708b5e8e01535aa89f9db6f1e7da841c97112c37
joshrenema2 <60946895+joshrenema2@users.noreply.github.com>
2021-01-31 08:39:27 -0800

    completed FoodInfo

--- src/comps/FoodInfo/index.js
+++ src/comps/FoodInfo/index.js
@@ -3,23 +3,52 @@ import styled from 'styled-components';
 
 const Container = styled.div`
 margin-top:100px;
-width: 375px;
-height: 750px;
 border: 1px solid red;
 border-radius: 33.5px 33.5px 0px 0px;
+position:absolute;
+padding-bottom:52px;
 `
 
 const Header = styled.div`
+display:flex;
+align-items:center;
+justify-content:space-between;
+margin: 26px 26px 0 26px;
 
+.food_name{
+    font-family: Pier Sans;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 30px;
+    line-height: 36px;
+}
+.expiry_date{
+    display:flex;
+    margin-top:5px;
+}
+.expiry{
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 16px;
+    opacity:0.5;
+    margin-right:10px;
+}
+.dot{
+    width: 16px;
+    height: 16px;
+    background: #70DA40;
+    border-radius:50%;
+}
 `
 
 const Macros = styled.div`
 width:auto;
-border: 1px solid red;
 display:flex;
 flex-direction:row;
 justify-content:center;
-margin: 26px 0;
+margin: 26px 26px 0 26px;
 & > div {
     width: 94px;
     height: 58px;
@@ -54,10 +83,77 @@ margin: 26px 0;
 `
 
 const StorageInfo = styled.div`
-
+margin: 26px 26px 0 26px;
+.info_cont{
+    margin-bottom:13px;
+}
+.info_header{
+    font-family: Pier Sans;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 22px;
+    letter-spacing: 0em;
+    text-align: left;
+    margin-bottom:5px;
+}
+.info_text{
+    font-family: Pier Sans;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 17px;
+    letter-spacing: 0em;
+    text-align: left;
+    opacity:0.5;
+}
 `
 
 const Nutrition = styled.div`
+border-radius: 16px;
+background-color: #F6F6FB;
+margin: 26px 26px 0 26px;
+padding-bottom:13px;
+.nutrition_head{
+    font-family: Pier Sans;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 22px;
+    letter-spacing: 0em;
+    text-align: left;   
+    padding: 13px 0px 13px 26px;
+    display:flex;
+}
+.dark_grey{
+    font-family: Pier Sans;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 14px;
+    letter-spacing: 0em;
+    text-align: left;
+    height:34px;
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    padding:0px 26px;
+    background-color: #EDEDED;
+}
+.light_grey{
+    font-family: Pier Sans;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 14px;
+    letter-spacing: 0em;
+    text-align: left;
+    height:34px;
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    padding:0px 26px;
+}
 
 `
 
@@ -66,12 +162,13 @@ const Nutrition = styled.div`
 const FoodInfo = () => {
 
     return <Container>
-        <StorageInfo>
-            <div>
-                <div></div>
-                <div></div>
+        <Header>
+            <div className="food_name">Cucumber</div>
+            <div className="expiry_date">
+                <div className="expiry">expires in 20 days</div>
+                <div className="dot"></div>
             </div>
-        </StorageInfo>
+        </Header>
         <Macros>
             <div>
                 <div className="macro_number">10</div>
@@ -86,6 +183,33 @@ const FoodInfo = () => {
                 <div className="macro_name">Fat</div>
             </div>
         </Macros>
+        <StorageInfo>
+            <div className="info_cont">
+                <div className="info_header">Amount</div>
+                <div className="info_text">3 Cucumbers currently stored</div>
+            </div>
+            <div className="info_cont">
+                <div className="info_header">Item Storage</div>
+                <div className="info_text">Find it in the Fridge</div>
+            </div>
+            <div className="info_cont">
+                <div className="info_header">Shelf</div>
+                <div className="info_text">Find it in the Produce Shelf</div>
+            </div>
+        </StorageInfo>
+        <Nutrition>
+            <div className="nutrition_head">
+                Nutrition Facts
+            </div>
+            <div className="dark_grey">
+                <div>Calories</div>
+                <div>200g</div>
+            </div>
+            <div className="light_grey">
+                <div>Calories</div>
+                <div>200g</div>
+            </div>
+        </Nutrition>
     </Container >
 }
 
