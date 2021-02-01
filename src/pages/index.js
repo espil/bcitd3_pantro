8d1e1aef969fa1ed385313375dd0443b4683a641
= <=>
2021-02-01 00:20:14 -0800

    jordan comps done, finishing java

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import FAB from 'comps/FAB';
 import Shelves from 'comps/Shelves';
 import FoodInfo from 'comps/FoodInfo';
+import Dropdown from 'comps/Dropdown'; 
+import Slider from 'comps/Slider';
+import IconSelect from 'comps/IconSelect'; 
 import styled from 'styled-components';
 
 import AddButton from 'comps/adbutton';
@@ -19,11 +22,14 @@ const Container = styled.div`
 const Main = () => {
 
   return <Container>
-    <FoodInfo />
+    {/* <IconSelect /> */}
+    {/* <Slider /> */}
+    <Dropdown />
+    {/* <FoodInfo /> */}
     {/* <AddButton />
     <DescInp /> */}
     {/* <Bbut /> */}
-    <BrBut />
+    {/* <BrBut /> */}
   </Container>
 
 
