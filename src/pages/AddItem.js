ac79736491db8722fda1a1e995b855b06f1bc9c1
ESPIL_CEIL <quadclipse@gmail.com>
2021-02-05 23:34:55 -0800

    Final for midterm

--- src/pages/AddItem.js
+++ src/pages/AddItem.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+  
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import BrBut from 'comps/BackButtonC';
 import AddButton from 'comps/AddButton';
 import Input from 'comps/Input';
-import Dropdown from 'comps/Dropdown';
 import { Link } from "react-router-dom";
+import restaurant from '../icons/restaurant.svg';
+
 import DatePicker from 'react-date-picker';
 
 const items = require("../fakeDatabase.json");
+const getItems = () => fetch(items).then(res => res.json());
 
 const Container = styled.div`
 display:flex;
@@ -27,30 +30,89 @@ padding:26px;
 }
 `;
 
+const DropdownSelect = styled.select`
+    width: 327px;
+    height: 42px;
+    background-color:#F6F6FB;  
+    color: #000;
+    font-size: 14px;
+    cursor: pointer;
+    border-radius:13px;
+    border:none;
+    padding:10px;
+    font-family: Pier Sans;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 200;
+    line-height: 19px;
+    letter-spacing: 0em;
+    text-align: left;
+    border:none;
+    padding-right: 16px;
+    -webkit-box-sizing: border-box; 
+`;
+
+const DropdownOption = styled.option`
+    min-height: 133px; 
+    max-width: 323px; 
+    background-color: #F6F6FB;
+    font-size: 16px;
+    font-weight: bold;
+    border-radius: 13px; 
+    &:hover {
+        background-color: #C7C7C7; 
+        transition: 0.3s; 
+    }
+`;
+
+const TopText = styled.p`
+    color: black; 
+    font-weight: bold; 
+    margin-left: 0.5vw;
+    user-select: none; 
+`;
 const AddItem = () => {
 
+  const [items, setItems] = useState();
+
+  useEffect(() => {
+    getItems().then(data => setItems(data));
+  }, []);
+
+  const [itemname] = useState([]);
+  const [amount] = useState([]);
+  const [shelf] = useState([]);
+  const [storage] = useState([]);
   const [expiry, onChange] = useState(new Date());
 
   return <div>
     
-    {items.map(o => <Container>
+    <Container>
 
-    <Link to="/Home"><BrBut></BrBut></Link>
-
+    <BrBut></BrBut>
     <div className="header">Add an Item</div>
-    <Input header="Item Name" value={itemname}/>
-    <div className="header"> Expiry Date (dd/mm/yyyy) </div>
-    <DatePicker
-        onChange={onChange}
-        value={expiry}
-      />
+    <Input header="Item Name" value={itemname} />
+    <DatePicker type="date" header="Expiry Date (dd/mm/yyyy)" value={expiry} />
     <Input header="Amount" value={amount}/>
-    <Dropdown header="Shelf" value={shelf}/>
-    <Dropdown header="Storage" value={Storage}/>
+    <TopText>Shelf</TopText>
+    <DropdownSelect value={shelf}>
+      <DropdownOption>None</DropdownOption>
+      <DropdownOption>Carbs</DropdownOption>
+      <DropdownOption>Canned Goods</DropdownOption>
+      <DropdownOption>Junk Food</DropdownOption>
+      <DropdownOption>Seafood</DropdownOption>
+    </DropdownSelect>
+    <TopText>Storage</TopText>
+    <DropdownSelect value={storage}>
+      <DropdownOption>None</DropdownOption>
+      <DropdownOption>Carbs</DropdownOption>
+      <DropdownOption>Freezer</DropdownOption>
+      <DropdownOption>Pantry</DropdownOption>
+    </DropdownSelect>
     <Link to="/">
-      <AddButton></AddButton>
+      <AddButton image={restaurant}></AddButton>
     </Link>
-  </Container>)}
+  </Container>)
 
   </div>
   }
